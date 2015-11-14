30ae4656135191664b46750a4686af07ba9e1813
Sean Macdonald <sean.macdonald@stjoseph.com>
2015-11-13 22:53:16 -0500

    first commit

--- app/scripts/dbx.js
+++ app/scripts/dbx.js
@@ -1,59 +1,95 @@
-;(function(G,undefined){
-	"use strict";
-	function DBX(dbname){
-
-		this.indexedDB = G.indexedDB || G.mozIndexedDB || G.webkitIndexedDB || G.msIndexedDB;
-		this.IDBTransaction = G.IDBTransaction || G.webkitIDBTransaction || G.msIDBTransaction;
-		this.IDBKeyRange = G.IDBKeyRange || G.webkitIDBKeyRange || G.msIDBKeyRange;
-
-		this.db = {
-			name: dbname
+"use strict";
+function DBX(dbname,schema){
+	/*
+	window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+	window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+	window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+	*/
+	//	sanity checking
+	this.db = {};
+	if (typeof schema === 'object') {
+		if ("version" in schema) {
+			this.version(schema.version);
 		}
-		return this;
+		this.schema(schema);
 	}
-	DBX.prototype.version = function(version) {
-		if (typeof version === 'number' && Math.floor(version) === version && version > 0 ) {
-			this.db.version = version;
-			return this;
-		} else {
-			throw new Error('version number must be a simple unsigned integer greater than 0');
+	if (typeof dbname === 'string') {
+	this.db.name = dbname;
+	} else {
+		throw new Error('wrong db name type');
+	}
+	return this;
+};
+
+DBX.prototype.version = function(version) {
+	if (typeof version === 'number' && Math.floor(version) === version && version > 0 ) {
+		this.db.version = version;
+		return this;
+	} else {
+		throw new Error('version number must be a simple unsigned integer greater than 0');
+	}
+};
+DBX.prototype.schema = function(schema) {
+	if (schema.constructor.name === 'Object') {
+		if ( ("version" in schema) && !("version" in this.db) ) {
+			this.version(schema.version);
 		}
-	};
-	DBX.prototype.schema = function(schema) {
-		if (schema.constructor.name === 'Object') {
-			this.db.schema = schema;
-			return this;
-		} else {
-			throw new Error('Wrong type for schema');
-		}
-	};
-	DBX.prototype.open = function() {
-		var dbx = this;
-		return new Promise(function(resolve, reject){
-			var request = this.indexedDB.open( dbx.db.name, dbx.db.version );
-			var cool = function(ev) {
-				resolve(ev.target.result);
+		this.db.schema = schema;
+		return this;
+	} else {
+		throw new Error('Wrong type for schema');
+	}
+};
+DBX.prototype.open = function(stores,mode) {
+	var dbx = this;
+	var myIDBRequest = window.indexedDB.open( dbx.db.name, dbx.db.version );
+	return new Promise(function(resolve, reject){
+		
+		var cool = function(ev) {
+			var db = ev.target.result;
+			dbx.db.instance = db;
+			var trans = db.transaction(stores,mode);
+			resolve(trans);
+			/*
+			trans.oncomplete = function(ev) {
+				resolve( ev.target );
 			}
-			var shitty = function(ev) {
-				console.log(ev);
-				reject(ev);
-				//throw new Error('something super shitty happened');
-			};
-			var upgrade = function(ev) {
-				return new Promise(function(resolve,reject){
-					var db = ev.target.result;
-
-				});
-			};
-			request.onsuccess 		= cool;
-			request.onerror 		= shitty;
-			request.onblocked 		= shitty;
-			request.onupgradeneeded = upgrade;
-			};
-		});
-	};
-	DBX.prototype.upgrade
-
-
-	return DBX;
-})(window);
+			*/
+		}
+		var shitty = function(ev) {
+			console.log(ev);
+			reject(event.target.errorCode);
+		};			
+		var upgrade = function(ev) {
+			return new Promise(function(resolve,reject){
+				var db = ev.target.result;
+				var num_stores = Object.keys(dbx.db.schema.stores).length;
+				for (var storename in dbx.db.schema.stores ) {
+					dbx.db.schema.stores[storename].primaryKey = dbx.db.schema.stores[storename].primaryKey || {};
+					if ( db.objectStoreNames.contains(storename) ) {
+						db.deleteObjectStore(storename);
+					}
+					var this_store = db.createObjectStore( storename, dbx.db.schema.stores[storename].primaryKey );
+					if ("indexes" in dbx.db.schema.stores[storename]) {
+						for (var keyname in dbx.db.schema.stores[storename].indexes) {
+							this_store.createIndex(keyname,keyname,dbx.db.schema.stores[storename].indexes[keyname]);
+						}
+					}
+				}				
+				this_store.transaction.oncomplete = function(ev){
+					resolve(ev.target.result);
+				};
+				this_store.transaction.onerror = function(ev){
+					reject(ev);
+				};
+			});
+		};
+		myIDBRequest.onsuccess 		= cool;
+		myIDBRequest.onerror 		= shitty;
+		myIDBRequest.onblocked 		= shitty;
+		myIDBRequest.onupgradeneeded= upgrade;
+	});
+};
+DBX.prototype.startTransaction = function(stores, mode){
+	return this.db.instance.transaction( stores, mode );
+};
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,28 +1,12 @@
 "use strict";
-var schema {
 
-};
-
-var conn = new DBX('db01').version(1).schema(database_schema);
-
-conn.open().then( dbx.startTransaction(['store01'],'readwrite') ).then(function(transaction){
-	//	here we do what we want, and return a promise when we want.
-	var store = transaction.objectStore('store01'),
-		request,
-		cool = function(row_id) { console.log(row_id) },
-		shitty = function(err) { console.log('Aw SNAP!',err) };
-
-	for (var i=0;i<100;i++) {
-		var row = {
-			id: i,
-			foo: G.performance.now(),
-			goo: ( Math.random() * Number.MAX_SAFE_INTEGER ).toString(16);
-		};
-		request = store.put(row);
-		request.onsuccess = cool;
-		request.onerror = shitty;
-	}
-
-	transaction.oncomplete = function(){};
-
+var db = new Dexie('numops');
+db.version(1).stores({
+	'people': '++id,name,dob'
+});
+db.open();
+db.people.add({name: "barb", dob: 3243}).then(function(){
+	db.people.where("dob").between(100,56767).each(function(row){
+		console.log(row);
+	});
 });
